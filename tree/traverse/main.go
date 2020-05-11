b0f57ccad14e99c4974297de33cde174ac8c9420
hjlzg <hjlzg@outlook.com>
2020-05-11 08:31:09 +0800

    add tree traver

--- tree/traverse/main.go
+++ tree/traverse/main.go
@@ -16,6 +16,16 @@ func main(){
 
 	fmt.Println()
 	rightTraverse(tree)
+
+	fmt.Println()
+	treeHigh:=treeHigh(tree)
+	fmt.Printf("树的高度：%v",treeHigh)
+
+	fmt.Println()
+	treeHigh2:=treeHighByQueue(tree)
+	fmt.Printf("树的高度(非递归)：%v",treeHigh2)
+
+
 }
 
 func createTree() *TreeNode{
@@ -29,6 +39,7 @@ func createTree() *TreeNode{
 	return sTree
 }
 
+//前序遍历
 func leftTraverse(t *TreeNode){
 	if t==nil{
 		return
@@ -39,6 +50,7 @@ func leftTraverse(t *TreeNode){
 	leftTraverse(t.right)
 }
 
+//中序遍历
 func middleTraverse(t *TreeNode){
 	if t==nil{
 		return
@@ -49,6 +61,7 @@ func middleTraverse(t *TreeNode){
 	middleTraverse(t.right)
 }
 
+//后序遍历
 func rightTraverse(t *TreeNode){
 	if t==nil{
 		return
@@ -57,4 +70,46 @@ func rightTraverse(t *TreeNode){
 	rightTraverse(t.left)
 	rightTraverse(t.right)
 	fmt.Printf("%v ",t.val)
+}
+
+//递归方法实现
+func treeHigh(t *TreeNode) int{
+	if t==nil{
+		return 0
+	}
+
+	leftTreeHigh:=treeHigh(t.left)
+	rightTreeHigh:=treeHigh(t.right)
+
+	if leftTreeHigh>rightTreeHigh{
+		return leftTreeHigh+1
+	}else{
+		return rightTreeHigh+1
+	}
+}
+
+func treeHighByQueue(t *TreeNode) int{
+	if t==nil{
+		return 0
+	}
+
+	layers:=0
+	nodes:=[]*TreeNode{t}
+	for len(nodes)>0{
+		layers++
+		size:=len(nodes) //每层的节点数
+		count:=0
+		for count<size{
+			count++
+			curNode:=nodes[0]
+			nodes=nodes[1:]
+			if curNode.left!=nil{
+				nodes=append(nodes,curNode.left)
+			}
+			if curNode.right!=nil{
+				nodes=append(nodes,curNode.right)
+			}
+		}
+	}
+	return layers
 }
